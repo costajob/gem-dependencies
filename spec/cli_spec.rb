7ef4e7a0450bab0e7bae6f8b328ecf63cf0eaf2f
costajob <costajob@gmail.com>
2017-09-20 15:33:53 +0200

    Mike - fixed spec

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -10,12 +10,12 @@ describe Lapidarius::CLI do
 
   it "must print runtime dependencies" do
     Lapidarius::CLI.new(%w[--gem=sinatra], io).call(Mocks::Command)
-    io.string.must_equal "\nsinatra (1.4.7)              3\n------------------------------\nrack (~> 1.5)\nrack-protection (~> 1.4)\ntilt (< 3, >= 1.3)\n\n"
+    io.string.must_equal "\nsinatra (1.4.7)             \e[1m3\e[0m\n------------------------------\nrack (~> 1.5)\nrack-protection (~> 1.4)\ntilt (< 3, >= 1.3)\n\n"
   end
 
   it "must print runtime dependencies recursively" do
     Lapidarius::CLI.new(%w[--gem=sinatra --recursive], io).call(Mocks::Command)
-    io.string.must_equal "\nsinatra (1.4.7)              3\n------------------------------\nrack (~> 1.5)\nrack-protection (~> 1.4)\n      rack (>= 0)\ntilt (< 3, >= 1.3)\n\n"
+    io.string.must_equal "\nsinatra (1.4.7)             \e[1m3\e[0m\n------------------------------\nrack (~> 1.5)\nrack-protection (~> 1.4)\n      rack (>= 0)\ntilt (< 3, >= 1.3)\n\n"
   end
 
   it "must warn about missing gems" do
